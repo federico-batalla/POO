4214d3a4ed42f5392d8b7bd42705037640cb9078
Federico <federicobatalla@tdf.edu.ar>
2022-09-19 23:55:55 -0300

    ejercicio terminado

--- 3clase/rectangulo.ts
+++ 3clase/rectangulo.ts
@@ -25,19 +25,19 @@ class Rectangulo {
     }
 
     public calcularArea(): number {
-        return this.base * this.altura
+        return this.base * this.altura;
     }
 
     public compararRectangulo(rectangulo2: Rectangulo): number {
         let areaRectangulo1 = this.base * this.altura;
         let areaRectangulo2 = rectangulo2.calcularArea();
         if (areaRectangulo1 > areaRectangulo2) {
-            return 1
+            return 1;
         } else {
             if (areaRectangulo1 < areaRectangulo2) {
-                return -1
+                return -1;
             } else {
-                return 0
+                return 0;
             }
         }
     }
@@ -46,15 +46,15 @@ class Rectangulo {
         if(this.altura == this.base){
             return true;
         }else{
-            return false
+            return false;
         }
     }
 
     public estaAcostado():boolean{
         if(this.altura > this.base){
-            return false
+            return false;
         }else{
-            return true
+            return true;
         }
     }
 
@@ -76,8 +76,8 @@ console.log("compara rcetangulo 1 con rectangulo 2 : "+rectangulo1.compararRecta
 console.log("compara rcetangulo 1 con rectangulo 3 : "+rectangulo1.compararRectangulo(rectangulo3));
 console.log("compara rcetangulo 1 con rectangulo 4 : "+rectangulo1.compararRectangulo(rectangulo4));
 
-console.log( "rcetangulo 4 es cuadrado? : "+rectangulo4.esCuadrado());
+console.log( "rectangulo 4 es cuadrado? : "+rectangulo4.esCuadrado());
 console.log( "rectangulo 1 es cuadrado? : "+rectangulo1.esCuadrado());
 
 console.log ("rectangulo 2 esta acostado? : "+rectangulo2.estaAcostado());
-console.log ("rcetangulo 1 esta Acostado?"+rectangulo1.estaAcostado());
+console.log ("rcetangulo 1 esta Acostado? :"+rectangulo1.estaAcostado());
