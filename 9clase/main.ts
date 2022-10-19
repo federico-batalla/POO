245f5a181f44e953b7d1a1d3b99031cb1265f16e
fede <federicobatalla@tdf.edu.ar>
2022-10-19 10:53:39 -0300

    Ejercicio Terminado

--- 9clase/main.ts
+++ 9clase/main.ts
@@ -9,6 +9,7 @@ let cliente2:Cliente = new Cliente("Milena","Gonzales",52899099);
 let cuentaCorriente:CuentaCorriente = new CuentaCorriente(cliente1);
 let cuentaAhorro:CuentaAhorro = new CuentaAhorro(cliente2,2.5);
 
+console.log("-------Prueba Cuenta Corriente-------");
 
 console.log(cuentaCorriente.toString());
 
@@ -21,8 +22,33 @@ cuentaCorriente.actualizarSaldo();
 console.log(cuentaCorriente.toString());
 
 console.log("retiro 345");
-cuentaCorriente.retirar(345);
+console.log(cuentaCorriente.retirar(345));
 console.log(cuentaCorriente.toString());
 
+console.log("retiro 2000");
+console.log(cuentaCorriente.retirar(2000));
+console.log(cuentaCorriente.toString());
+
+
+console.log("-------Prueba Cuenta Ahorro-------");
+
+console.log(cuentaAhorro.toString());
+
+console.log("ingreso 5000");
+cuentaAhorro.ingresar(5000);
+console.log(cuentaAhorro.toString());
+
+
+console.log("actualizo saldo interes 5%");
+cuentaAhorro.actualizarSaldo(5);
+console.log(cuentaAhorro.toString());
+
+console.log("retiro 3000");
+console.log(cuentaAhorro.retirar(2000));
+console.log(cuentaAhorro.toString());
+
+console.log("retiro 3250");
+console.log(cuentaAhorro.retirar(3250));
+console.log(cuentaAhorro.toString());
 
 
